3418b0a73a9dc79f45dbca62031e0f3de590ea35
Your Name <you@example.com>
2023-12-10 15:22:07 +0100

    fiill

--- src/client/WebServiceClient.java
+++ src/client/WebServiceClient.java
@@ -5,21 +5,38 @@ import classes.BanqueService;
 import classes.Compte;
 import classes.SecondService;
 
+import java.util.Scanner;
+
 public class WebServiceClient {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
         // Create instances of the web services
         BanqueService banqueService = new BanqueService();
         SecondService secondService = new SecondService();
+
+        System.out.print("Enter code for BanqueService: ");
+        int banqueCode = scanner.nextInt();
+        Compte compte = banqueService.getCompte(banqueCode) ;
+        System.out.println("Banque compte de num " + banqueCode + "is " + compte.getCode());
+
 
         // Invoke methods on BanqueService
         double conversionResult = banqueService.conversion(100.0);
         System.out.println("BanqueService Conversion Result: " + conversionResult);
 
-        Compte compte = banqueService.getCompte(1) ;
-        System.out.println("Banque compte de num 1 is " + compte.getCode());
 
-        // Invoke methods on SecondService
-        String operationResult = secondService.performOperation("World!");
+
+        // Get user input for SecondService
+        System.out.print("Enter input for SecondService: ");
+        String secondServiceInput = scanner.next();
+        // Invoke methods on SecondService with dynamic input
+        String operationResult = secondService.performOperation(secondServiceInput);
         System.out.println("SecondService Result: " + operationResult);
+
+        // Close the scanner
+        scanner.close();
     }
 }
--- src/serveeer/SecondServiceServer.java
+++ src/serveeer/SecondServiceServer.java
@@ -7,10 +7,8 @@ import javax.xml.ws.Endpoint;
 public class SecondServiceServer {
         public static void main(String[]args)
         {
-
                 Endpoint.publish("http://localhost:9091/second", new SecondService());
                 System.out.println("SecondService deployed on http://localhost:9091/second/?wsdl");
-
         }
     // Publish the SecondService
 
